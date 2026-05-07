6691aa38e793bfab9a62d218f3886e60b22ab4d9
Ryan Ling <youremail@gmail.com>
2026-05-06 21:58:05 -0400

    fix api function url

--- api/generate.js
+++ api/generate.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    const response = await fetch('/api/generate', {
+    const response = await fetch('https://api.anthropic.com/v1/messages', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
